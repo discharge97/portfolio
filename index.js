b19a94962ec0d05657d31d70b8b52666dccd47f9
Alexander <i00jko@outlook.com>
2021-05-29 13:02:23 +0200

    initial project

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("simple").onclick = function () {
+    window.location.href = './simple/index.html';
+}
+
+document.getElementById("advanced").onclick = function () {
+    window.location.href = './advanced/index.html';
+}
